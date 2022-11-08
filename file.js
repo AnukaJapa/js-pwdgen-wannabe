3aa6d5e34422816516410f0b0f3b198413160a5e
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-08 16:29:00 +0100

    creato funzione di calc in js.

--- file.js
+++ file.js
@@ -1,3 +1,28 @@
+let firstNameDocument = document.getElementById("firstName");
+let lastNameDocument =document.getElementById("lastName");
+let favoriteColorDocument =document.getElementById("favoriteColor");
+let number1Document = document.getElementById("number1");
+let number2Document = document.getElementById("number2");
+let firstName;
+let lastName;
+let favoriteColor;
+let number1;
+let number2;
+let generatedPassword;
+
+
+function calc(){
+    firstName = firstNameDocument.value;
+    lastName=lastNameDocument.value;
+    favoriteColor=favoriteColorDocument.value;
+    number1 = number1Document.value;
+    number2 =number2Document.value;
+    let division = number1/number2;
+
+    generatedPassword = firstName + lastName + favoriteColor + division; 
+    console.log(generatedPassword);
+}
+
 // let firstName =prompt("Please tell us your firstname");
 // let lastName =prompt("Please give us your lastname");
 // let favoriteColor = prompt("plase tell us your favorite color");

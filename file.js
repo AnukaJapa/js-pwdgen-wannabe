271e3192b39e5affaff7cfb2ee3eeaaad5ef97f6
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-08 13:58:29 +0100

    completato esercizio base in js

---
+++ file.js
@@ -0,0 +1,12 @@
+let firstName =prompt("Please tell us your firstname");
+let lastName =prompt("Please give us your lastname");
+let favoriteColor = prompt("plase tell us your favorite color");
+let firstNumber = prompt("plase give us some number");
+let secondNumber = prompt("plase give us some number");
+
+let finalNumber = firstNumber/secondNumber;
+let password = firstName+lastName+favoriteColor + finalNumber;
+
+console.log(password);
+
+
